bc97370bc725b5b231bb9ae2033df32d80c3796b
HC <lovelifeloveyoumore@gmail.com>
2017-05-03 01:01:08 +0800

    test optimization

--- tdd2/test/input.test.js
+++ tdd2/test/input.test.js
@@ -18,12 +18,9 @@ describe('app 输入部分测试:', function() {
 		wrapper_mount = null;
 	});
 
-	it('输入部分应该有子节点(子节点数大于0)', function() {
-		expect(wrapper_shallow.children()).to.have.length.above(0);
-	});
-
-	it('有一个输入框', function() {
+	it('输入部分应该有一个输入框和按钮', function() {
 		expect(wrapper_shallow.children('input')).to.have.length.above(0);
+		expect(wrapper_shallow.children('button')).to.have.length.above(0);
 	});
 
 	it('输入框是文本输入框', function() {
@@ -34,7 +31,7 @@ describe('app 输入部分测试:', function() {
 		expect(wrapper_shallow.children('input')).to.have.attr('placeholder').equal('请输入待办事项');
 	});
 
-	it('输入框可以输入任何值', function() {
+	it('用户在输入框输入数字111，输入框显示111', function() {
 		let input = wrapper_mount.find('input');
 		expect(input).to.have.prop('onChange');
 
@@ -42,30 +39,17 @@ describe('app 输入部分测试:', function() {
 		expect(input).to.have.value('111');
 	});
 
-	it('有一个按钮', function() {
-		expect(wrapper_shallow.children('button')).to.have.length.above(0);
-	});
-
 	it('按钮为确认按钮', function() {
 		expect(wrapper_shallow.children('button')).to.have.attr('type').equal('button');
 		expect(wrapper_shallow.children('button')).to.have.text('确定');
 	});
 
-	it('按钮有点击事件', function() {
-		// given
-		let btn_add_todo = wrapper_shallow.find('button');
-		// when
-		
-		// then
-		expect(btn_add_todo).to.have.prop('onClick');
-	});
-
-	it('按钮点击后input会清空', function() {
+	it('按钮点击后，输入框会清空', function() {
 		// given
 		let btn_add_todo = wrapper_mount.find('button');
 		let input = wrapper_mount.find('input');
-		// when
 		wrapper_mount.simulate('change', {target: {val: 122223}});
+		// when
 		btn_add_todo.simulate('click');
 		// then
 		expect(input).to.have.value('');
